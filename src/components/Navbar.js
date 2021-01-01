9f507f3f8c2ee7e1e6e5228b963cf990925932d2
Tessa Woodard <tessawoodard@outlook.com>
2020-12-31 19:59:33 -0500

    sidebar wip

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,23 @@ const Nav = () => {
           <Link to="/">
             <img src={logo} alt="comfy sloth" />
           </Link>
+          <button type="button" className="nav-toggle">
+            <FaBars />
+          </button>
         </div>
+        <ul className="nav-links">
+          {links.map((link) => {
+            const { id, text, url } = link
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            )
+          })}
+          <li>
+            <Link to="/checkout">checkout</Link>
+          </li>
+        </ul>
         <CartButtons />
       </div>
     </NavContainer>
